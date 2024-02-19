21895ff2128eb0d7564a4e8f7c0a37dba6849e5c
Farelito <bakoufarel@gmail.com>
2024-02-19 15:22:35 +0000

    ccc

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,20 @@
-int main(){
-int n;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*scanf("%d", &n);*/
-if(n > 0 )
+int main(){
+int n = 0;
+
+scanf("%d", &n);
+if (n > 0 )
 {
-    printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }else if (n == 0)
 {
-     printf("%d is zero\n", n);
-     
+printf("%d is zero\n", n);
 }else
 {
-     printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
-
 return(0);
 }
