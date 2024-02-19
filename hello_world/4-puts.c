20f1434fb16b5a2f4ece31428951812a28093447
Farelito <bakoufarel@gmail.com>
2024-02-19 13:27:48 +0000

    cc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc $CFILE -E -o c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *message="Programming is like building a multilingual puzzle";
+	puts(message);
+    return (0);
+}
