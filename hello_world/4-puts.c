8e9adb96ad94816cf340ec1f78bc71375297a89d
Farelito <bakoufarel@gmail.com>
2024-02-19 13:30:44 +0000

    ccc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-const char *message="Programming is like building a multilingual puzzle";
-	puts(message);
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
