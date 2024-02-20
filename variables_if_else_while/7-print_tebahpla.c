4ba74a01651805eb53a593c7823bd23d1512779d
Farelito <bakoufarel@gmail.com>
2024-02-20 14:24:13 +0000

    ccc

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char lttr;
+for(lttr = 'z'; lttr >= 'a'; lttr--)
+putchar(lttr);
+putchar('\n');
+return (0);
+}
---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int hex;
+for(hex = 0; hex < 16; hex++)
+{
+if (hex < 10){
+putchar('0' + hex);
+}
+else {
+putchar('a' + hex - 10);
+}
+}
+putchar('\n');
+return (0);
+}
