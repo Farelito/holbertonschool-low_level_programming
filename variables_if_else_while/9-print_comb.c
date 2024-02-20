26b4b1a4682f55469319684483dfcac79ae53b94
Farelito <bakoufarel@gmail.com>
2024-02-20 14:42:41 +0000

    cc

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int dgt; 
+for(dgt = 0; dgt <= 9; dgt++)
+{
+putchar((dgt % 10) + '0');
+if (dgt == 9){
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
