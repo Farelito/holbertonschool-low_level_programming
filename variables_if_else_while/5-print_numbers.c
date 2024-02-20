1045b8b5db4a3dd4ba3686984024ae2fc914b263
Farelito <bakoufarel@gmail.com>
2024-02-20 13:26:35 +0000

    c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int dgt;
+for(dgt = 0; dgt < 10; dgt++)
+printf("%d\n", dgt);
+return (0);
+}
