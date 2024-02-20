0fe210e49c49a9d83f6cb6ede82f9ea26fca7de2
Farelito <bakoufarel@gmail.com>
2024-02-20 13:40:57 +0000

    cccccccccc

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int dgt;
+for(dgt = 0; dgt < 10; dgt++)
+putchar(dgt + '0');
+putchar('\n');
+return (0);
+}
