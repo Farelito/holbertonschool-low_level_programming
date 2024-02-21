67076055a3c77c923b3d788e580347912b39b166
Farelito <bakoufarel@gmail.com>
2024-02-21 22:53:05 +0000

    pushing task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: the absolute value of int
+ */
+int _abs(int nmbr)
+{
+	if (nmbr >= 0)
+	{
+		return (nmbr);
+	}
+	return (-nmbr);
+}
