132391b85741c011004ed06808aaccbf1a64a670
Farelito <bakoufarel@gmail.com>
2024-02-21 11:03:52 +0000

    pushing task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	_putchar(alph);
+	}
+	_putchar('\n');
+
+}
---
+++ functions_nested_loops/1-alphabet.cy
@@ -0,0 +1,17 @@
+int main(void)
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	_putchar(alph);
+	}
+	_putchar('\n');
+
+}
