fc47079baecdc91ee2437e46ddbc9fcab8488aaf
bakoufarel@gmail.com <Farelito>
2024-02-22 15:57:07 +0000

    push to finish later task 8 jack

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints every minute of the day of Jack Bauer
+  *
+  * Return: ...
+  */
+
+void jack_bauer(void)
+{
+
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+	for (min = 0; min < 24; min++)
+	{
+		_putchar((hr / 10) + '0');
+		_putchar((hr % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+	}
+	_putchar('\n');
+
+}
