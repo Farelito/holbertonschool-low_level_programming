1d713798a163ecc13b496df552f82bb1eb255f1c
bakoufarel@gmail.com <Farelito>
2024-02-22 13:08:22 +0000

    push task 6 resolve

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,14 +2,18 @@
 
 /**
  * _abs - computes the absolute value of an integer
- *
+ * @nmbr: the int to check
  * Return: the absolute value of int
  */
+
 int _abs(int nmbr)
 {
-	if (nmbr >= 0)
+	if (nmbr < 0)
 	{
-		return (nmbr);
+		int abs_val;
+
+		abs_val = nmbr * -1;
+		return (abs_val);
 	}
-	return (-nmbr);
+	return (nmbr);
 }
