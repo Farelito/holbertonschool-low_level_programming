858dfb524b93f8a9432bfe37f225ac2a3af40e21
Farelito <bakoufarel@gmail.com>
2024-02-26 23:48:02 +0000

    task 9 checking

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints Fizz for multiples of 3,
+ * Buzz for multiples of 5, and FizzBuzz for multiples of both 3 and 5,
+ * otherwise prints the number.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int num;
