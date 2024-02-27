7867e7cbb7bb63c4dcf7042f5701d1a0373deaee
Farelito <bakoufarel@gmail.com>
2024-02-27 12:55:16 +0000

    task 0 betty check

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * reset_to_98 - updates the value pointed to by the parameter to 98
  * @n: a pointer to an int
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
