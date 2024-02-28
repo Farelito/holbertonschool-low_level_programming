d4ef864ff2d7ee08360c6d98d1bc27cd29c4b931
Farelito <bakoufarel@gmail.com>
2024-02-28 09:24:57 +0000

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-* puts2 - Write a function that prints every other character of a string
-*starting with the first character, followed by a new line.
-* @s: This pointer point to a char or string
-*str: pointer to the string
-*/
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: the string to be printed
+ */
 void puts2(char *str)
 {
 	int i = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if(i % 2 == 0)
+	if (i % 2 == 0)
 	{
 		_putchar(str[i]);
 	}
