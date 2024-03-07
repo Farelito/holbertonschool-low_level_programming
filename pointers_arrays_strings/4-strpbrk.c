fda167350e5a87f2f20f45369a773cd552e3e93b
Farelito <bakoufarel@gmail.com>
2024-03-07 09:02:12 +0000

    check

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,24 +11,24 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	if (s == NULL || accept == NULL)
+	{
+		fprintf(stderr, "Null pointer detected\n");
+		return (0);
+	}
+	while (*s != '\0')
+	{
+		const char *a = accept;
+	while (*a != '\0')
+	{
+		if (*s == *a)
+		{
+			return (s);
+		}
+		a++;
+	}
+	s++;
+	}
+	return (NULL);
 
-      if (s == NULL || accept == NULL)
-    {
-        fprintf(stderr, "Null pointer detected\n");
-        return(0);
-    }
-
-    while (*s != '\0')
-    {
-        const char *a = accept;
-        while (*a != '\0'){
-            if ( *s == *a)
-            {
-                return s;
-            }
-            a++;
-        }
-        s++;
-    }
-    return NULL;
 }
