cb5467b886db581374f6f480f20789d30526d679
Farelito <bakoufarel@gmail.com>
2024-03-07 12:11:02 +0000

    task check projet 3 on the week

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,29 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* main - Prints the multiplication of two args numbers
-* @argc: argument count
-* @argv: argument vector
-* Return: Always zero
-*/
+  * main - Prints the multiplication of two args numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-int number1 = 0;
-int number2 = 0;
-
-if (argc == 2)
+int number1 = 0, number2 = 0;
+if (argc == 3)
 {
-number2 = atoi(argv[2]);
 number1 = atoi(argv[1]);
-printf("%d\n", number1 * number2);
+number2 = atoi(argv[2]);
+printf("%d\n", number1 *number2);
 }
 else
 {
 printf("Error\n");
 return (1);
 }
-
 return (0);
 }
