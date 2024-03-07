d4a2e3b8410ae0d0403d6c5be8beb7fdf47e2592
Farelito <bakoufarel@gmail.com>
2024-03-07 11:04:45 +0000

    task

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
-*_sqrt_recursion - returns the natural square root of n
-*@n: Number Integer
-*Return: On success 1.
-*On error, -1 is returned, and errno is set appropriately.
-*/
-int _sqrt_recursion(int n)
-{
-	return (_sqrt2_recursion(n, 1));
-}
-
-/**
 *sqrt2 - Makes possible to evaluate from 1 to n
 *@a: same number as n
 *@b: number that iterates from 1 to n
@@ -26,3 +15,15 @@ int _sqrt2_recursion(int a, int b)
 		return (-1);
 	return (_sqrt2_recursion(a, b + 1));
 }
+
+/**
+*_sqrt_recursion - returns the natural square root of n
+*@n: Number Integer
+*Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt2_recursion(n, 1));
+}
+
