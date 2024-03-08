d22fadb8cc687bde7be518f738684811bd09040a
Farelito <bakoufarel@gmail.com>
2024-03-08 15:07:45 +0000

    betty

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,12 +3,16 @@
 #include <string.h>
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: segment to return bytes from
- * @accept: the bytes to include
- * Return: the number of bytes in the initial segment of @s which consist only
- * of bytes from @accept
- */
+*_strpbrk - The _strpbrk() function locates the first
+* occurrence in the string s of any of the bytes in
+*the string accept
+*
+*@s: string where search is made
+*@accept: string where searched bytes are located
+*
+*Return:Returns a pointer to the byte in s that matches
+* one of the bytes in accept, or NULL if no such byte is found
+*/
 char *_strpbrk(char *s, char *accept)
 {
 if (s == NULL || accept == NULL)
