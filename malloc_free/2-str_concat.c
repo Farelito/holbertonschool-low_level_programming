a4e643749cc59524e388b34b1febb817fc294413
Farelito <bakoufarel@gmail.com>
2024-03-11 15:32:18 +0000

    task 2 doesn.t work

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,16 +12,21 @@ char *str_concat(char *s1, char *s2)
 {
 	char *array;
 
-	if ((s1 == NULL) && (s2 == NULL))
-		return (NULL);
-
 	array = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (array == NULL)
 		return (NULL);
 
-	strcat(array, s1);
-	strcat(array, s2);
+	if (s1 != NULL)
+    {
+        strcpy(array, s1);
+    }
+        
+    if (s2 != NULL)
+    {
+        strcat(array, s2);
+    }
+        
 
 	return (array);
 }
