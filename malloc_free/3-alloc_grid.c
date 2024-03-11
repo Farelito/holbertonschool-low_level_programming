b5570524860c98f8fb35b862df2aa123bc738dfc
Farelito <bakoufarel@gmail.com>
2024-03-11 14:54:06 +0000

    projet 1 done

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int **alloc_grid(int width, int height)
-{
+int **alloc_grid(int width, int height) {
     int i, j;
     int **array;
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
+    if (width <= 0 || height <= 0) {
+        return NULL;
     }
 
-    array = (int **)malloc(height + 1);
-     if (array == NULL)
-    {
-        return (NULL);
-    }
-
-     for (i = 0; i < height; i++)
-    {
-       array[i] = (int **)malloc(width + 1);
+    array = (int **)malloc(height * sizeof(int *));
     
-    if (array[i] == NULL)
-    {
-        for (j = 0; i < j; j++)
-        {
-            free(array[j]);
+    if (array == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < height; i++) {
+        array[i] = (int *)malloc(width * sizeof(int));
+
+        if (array[i] == NULL) {
+           
+            for (j = 0; j < i; j++) {
+                free(array[j]);
+            }
+            free(array);
+            return (NULL);
+        }
+
+        for (j = 0; j < width; j++) {
+            array[i][j] = 0;
         }
     }
-     free(array);
-    return NULL;
-    }
-    
 
-    for (i = 0; i < width; i++)
-    {
-        array[i][j] = 0;
-    }
-
-   
     return (array);
 }
