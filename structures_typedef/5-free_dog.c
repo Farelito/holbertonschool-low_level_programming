1e1735b28cdcfe757b181f5b5914661bf5923538
Farelito <bakoufarel@gmail.com>
2024-03-14 10:48:08 +0000

    betty

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include "dog.h"
+
+/**
+*free_dog - Frees memory allocated for a dog structure
+*@d: Pointer to the dog structure to free
+*This function frees the memory allocated for the fields
+*'name' and 'owner' of the given dog structure, as well
+*as the memory allocated for the dog structure itself.
+*/
 void free_dog(dog_t *d)
 {
-    if (d != NULL) 
-    {
-        free(d->name);
-        free(d->owner);
-        
-        free(d);
-    }
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+
+	free(d);
+	}
 }
