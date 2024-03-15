7319f49d3d2606ede26ab6ee72059b8863f71afc
Farelito <bakoufarel@gmail.com>
2024-03-15 14:01:27 +0000

    betty style

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,41 +10,40 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int i, j;
-    int **array;
+	int i, j;
+	int **array;
 
-    if (width <= 0 || height <= 0)
-    {
-        return NULL;
-    }
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    array = (int **)malloc(height * sizeof(int *));
+	array = (int **)malloc(height * sizeof(int *));
 
-    if (array == NULL)
-    {
-        return NULL;
-    }
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < height; i++)
-    {
-        array[i] = (int *)malloc(width * sizeof(int));
-        if (array[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-            {
-                free(array[j]);
-            }
-            free(array);
-            return NULL;
-        }
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(width * sizeof(int));
 
-        for (j = 0; j < width; j++)
-        {
-            array[i][j] = 0;
-        }
-    }
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
 
-    return (array);
+		return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	return (array);
 }
+
 
 
