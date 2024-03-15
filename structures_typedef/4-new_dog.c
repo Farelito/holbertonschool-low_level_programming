af677386f8604dd69762e0459a0d440072e3772e
Farelito <bakoufarel@gmail.com>
2024-03-15 13:43:08 +0000

    betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
-*new_dog - creates a new dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
-*Return: pointer to new dog
+* new_dog - creates a new dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+* Return: pointer to new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
@@ -42,3 +44,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
+
