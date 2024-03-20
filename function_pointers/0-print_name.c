a75ab8b45b409063eef75731655a54985c067b86
Farelito <bakoufarel@gmail.com>
2024-03-20 11:15:29 +0000

    checker

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
-* print_name - Prints a name
-* @name: The name to prints
-* @f: Pointer to function
+*print_name - Prints a name
+*@name: The name to prints
+*@f: Pointer to function
 *
-* Return: Nothing
+*Return: Nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
