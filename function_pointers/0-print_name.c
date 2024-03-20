64ca7adaf0b81318be36698fad5859cfc68a87b7
Farelito <bakoufarel@gmail.com>
2024-03-20 01:17:16 +0000

    task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
 *print_name - print name from a function pointer
 *@name: char string
@@ -8,6 +9,7 @@ void print_name(char *name, void (*f)(char *))
 {
 f(name);
 }
+
 
 
 
