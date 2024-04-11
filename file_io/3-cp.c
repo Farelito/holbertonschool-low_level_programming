e10d83fd26d729899e2ff6fbb72d18a7ffdacc74
Farelito <bakoufarel@gmail.com>
2024-04-11 10:15:11 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,77 +1,69 @@
-#include "main.h"
-#include <stdio.h>
-
-
-#define BUFFER_SIZE 1024
-
-
+#include "sort.h"
 /**
- * print_error - Prints an error message to standard error output
- * @message: The error message to print
- * @filename: The name of the file associated with the error
+ * conquer - partition and sort the partitions
+ * @array: the array to sort
+ * @size: the size of the array
+ * @lower_bound: the lower bound of the partition (inclusive)
+ * @upper_bound: the upper bound of the partition (inclusive)
+ *
+ * Return: the index of the new partition
  */
-void print_error_file(int file_from, int file_to, char *argv[])
+size_t conquer(int *array, size_t size, ssize_t lower_bound,
+		ssize_t upper_bound)
 {
-    if (file_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
+	ssize_t current_index = lower_bound;
 
+	while (current_index < upper_bound)
+	{
+		if (array[current_index] < array[upper_bound])
+		{
+			if (lower_bound != current_index)
+			{
+				array[lower_bound] ^= array[current_index];
+				array[current_index] ^= array[lower_bound];
+				array[lower_bound] ^= array[current_index];
+				print_array(array, size);
+			}
+			lower_bound += 1;
+		}
+		current_index += 1;
+	}
+	if (array[lower_bound] != array[upper_bound])
+	{
+		array[lower_bound] ^= array[upper_bound];
+		array[upper_bound] ^= array[lower_bound];
+		array[lower_bound] ^= array[upper_bound];
+		print_array(array, size);
+	}
+	return (lower_bound);
 }
 
 /**
- * main - The main function of the program
- * @argc: The number of command-line arguments
- * @argv: An array of pointers to the command-line arguments
- *
- * Return: 0 on success, or various error codes on failure
+ * divide - recursively partition the array
+ * @array: the array to sort
+ * @size: the size of the array
+ * @lower_bound: the lower bound of the partition (inclusive)
+ * @upper_bound: the upper bound of the partition (inclusive)
  */
-int main(int argc, char *argv[])
+void divide(int *array, size_t size, ssize_t lower_bound, ssize_t upper_bound)
 {
-    int file_from, file_to, error_cls_from, error_cls_to;
-    char buffer[BUFFER_SIZE];
-    ssize_t buff_read, buff_written;
+	ssize_t partition_index;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-        
-    file_from = open(argv[1], O_RDONLY);
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    print_error_file(file_from, file_to, argv);
+	if (lower_bound < upper_bound)
+	{
+		partition_index = conquer(array, size, lower_bound, upper_bound);
+		divide(array, size, lower_bound, partition_index - 1);
+		divide(array, size, partition_index + 1, upper_bound);
+	}
+}
 
-    buff_read = BUFFER_SIZE;
-     while (buff_read == BUFFER_SIZE) {
-        buff_read =  read(file_from, buffer, BUFFER_SIZE);
-        if (buff_written == -1) 
-            print_error_file(-1, 0, argv);
-		buff_written = write(file_to, buffer, buff_read);
-        
-        if (buff_written == -1)
-			print_error_file(0, -1, argv);
-    }
-    
-     error_cls_from = close(file_from);
-     if (error_cls_from == -1) 
-     {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit (100);
-    }
-
-    error_cls_to = close(file_to);
-    if (error_cls_to == -1) 
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit (100);
-    }
-
-    return (0);
+/**
+ * quick_sort - perform the quick sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array && size)
+		divide(array, size, 0, size - 1);
 }
