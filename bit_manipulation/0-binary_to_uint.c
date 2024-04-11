ac35a87fd03ac1b9ba83e692d9d0e7f9331d0c31
Farelito <bakoufarel@gmail.com>
2024-04-11 11:25:49 +0000

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <stddef.h>
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 characters.
+ *
+ * Return: The converted unsigned int number, or 0 if:
+ *      - b is NULL.
+ *      - One or more characters in the string b is not '0' or '1'.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len;
+	unsigned int conv;
+
+	conv = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len]; len++)
+	while (*b != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
+		conv = (conv << 1) + (*b - '0');
+		b++;
 	}
 
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (conv);
 }
+
